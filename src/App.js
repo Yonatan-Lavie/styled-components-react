1939691bee71ea1c4f33dea0937996e1523bb3ed
Yonatan Lavie <yonatan.lavie89@gmail.com>
2022-11-05 20:32:47 +0200

    use Theme Provider to pass theme object that will be available in the styled  objects

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { ThemeProvider } from 'styled-components';
 import { Header } from './conponenets/Header';
 import { Container } from './conponenets/styles/Container.styled';
 
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#ffffff',
+    footer: '#003333',
+  },
+};
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Container>
         <h1>Hello World</h1>
       </Container>
-    </>
+    </ThemeProvider>
   );
 };
 
