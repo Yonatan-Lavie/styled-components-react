68141117e2103c05673578d6db8aa15814e8fcc3
Yonatan Lavie <yonatan.lavie89@gmail.com>
2022-11-05 20:10:55 +0200

    Start - install styled-components - create styled Container - create Header component - create styled Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from './conponenets/Header';
+import { Container } from './conponenets/styles/Container.styled';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
