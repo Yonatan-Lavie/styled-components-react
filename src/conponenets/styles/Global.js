1f501379ab95935234ddc161dcc4786a3aedbbe5
Yonatan Lavie <yonatan.lavie89@gmail.com>
2022-11-05 21:39:00 +0200

    useing theme in the global styles.

--- src/conponenets/styles/Global.js
+++ src/conponenets/styles/Global.js
@@ -8,7 +8,7 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        background: #fff;
+        background: ${({ theme }) => theme.colors.body};
         color: hsl(192, 100%, 9%);
         font-family: 'Poppins', sans-serif;
         font-size: 1.15em;
